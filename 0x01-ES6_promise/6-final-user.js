52efda722fc2b9be1c600361796436f76b0f9bf4
zakisu250 <zakisu250@gmail.com>
2024-01-02 16:44:10 +0300

    Add handleProfileSignup function to handle user profile signup

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,14 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+function handleProfileSignup(firstName, lastName, fileName) {
+    return Promise.allSettled([
+        signUpUser(firstName, lastName),
+        uploadPhoto(fileName),
+    ]).then((results) => results.map(({ status, value, reason }) => ({
+        status,
+        value: status === 'fulfilled' ? value : reason,
+    })));
+}
+
+export default handleProfileSignup;
