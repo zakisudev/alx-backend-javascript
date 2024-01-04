149beeaf509acd1fd6eedd973f6434c80c9dd60c
zakisu250 <zakisu250@gmail.com>
2024-01-04 12:48:23 +0300

    Refactor block-scoped variables to use const

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,12 @@
 export default function taskBlock(trueOrFalse) {
-  var task = false;
-  var task2 = true;
+  const task = false;
+  const task2 = true;
 
   if (trueOrFalse) {
+    /* eslint-disable no-unused-vars */
     const task = true;
     const task2 = false;
+    /* eslint-enable no-unused-vars */
   }
 
   return [task, task2];
