f2d35435548a0a0a9292d2b8e442813bee8ce38f
zakisu250 <zakisu250@gmail.com>
2024-01-04 13:07:14 +0300

    Fix indentation in uploadPhoto function

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,5 @@
 function uploadPhoto(fileName) {
-    return Promise.reject(new Error(`${fileName} cannot be processed`));
+  return Promise.reject(new Error(`${fileName} cannot be processed`));
 }
 
 export default uploadPhoto;
