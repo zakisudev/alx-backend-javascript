e04b53eaf36a13393fc32b46a884761dc5f19a8a
zakisu250 <zakisu250@gmail.com>
2024-01-04 13:07:09 +0300

    Refactor signUpUser function to use ES6 object shorthand syntax

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,8 +1,8 @@
 function signUpUser(firstName, lastName) {
-    return Promise.resolve({
-        firstName: firstName,
-        lastName: lastName,
-    });
+  return Promise.resolve({
+    firstName,
+    lastName,
+  });
 }
 
 export default signUpUser;
