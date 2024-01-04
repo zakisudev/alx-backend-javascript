f5a13f8b4866e9799ef5ba34c0263c2a04d738e7
zakisu250 <zakisu250@gmail.com>
2024-01-04 14:49:18 +0300

    Fix type errors and formatting in HolbertonCourse class

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,13 +1,13 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
     if (typeof name !== 'string') {
-        throw TypeError('Name must be a string');
+      throw TypeError('Name must be a string');
     }
     if (typeof length !== 'number') {
-        throw TypeError('Length must be a number');
+      throw TypeError('Length must be a number');
     }
     if (!Array.isArray(students) || students.some(student => typeof student !== 'string')) {
-        throw TypeError('Students must be an array of strings');
+      throw TypeError('Students must be an array of strings');
     }
     this._name = name;
     this._length = length;
@@ -31,7 +31,7 @@ export default class HolbertonCourse {
 
   set length(newLength) {
     if (typeof newLength !== 'number') {
-       throw TypeError('Length must be a number');
+      throw TypeError('Length must be a number');
     }
     this._length = newLength;
   }
@@ -42,7 +42,7 @@ export default class HolbertonCourse {
 
   set students(newStudents) {
     if (!Array.isArray(newStudents) || newStudents.some(student => typeof student !== 'string')) {
-       throw TypeError('Students must be an array of strings');
+      throw TypeError('Students must be an array of strings');
     }
     this._students = newStudents;
   }
