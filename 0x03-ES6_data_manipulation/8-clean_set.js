ce495c58342e8e199c22041a3631c25408ee19f4
zakisu250 <zakisu250@gmail.com>
2024-01-08 11:46:05 +0300

    Add cleanSet function to manipulate sets

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+function cleanSet(set, startString) {
+  if (startString === '') return '';
+  return [...set]
+    .filter((item) => item.startsWith(startString))
+    .map((item) => item.slice(startString.length))
+    .join('-');
+}
+
+export default cleanSet;
