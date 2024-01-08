8b955b73ae949febe2b5214609605db36f779ebc
zakisu250 <zakisu250@gmail.com>
2024-01-08 18:37:21 +0300

    Refactor cleanSet function to improve readability

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,7 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') return '';
+  if (startString === '') {
+    return '';
+  }
 
   let result = '';
   for (const item of set) {
