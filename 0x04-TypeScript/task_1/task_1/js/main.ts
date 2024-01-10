f58719b90cca57179a6797be6b63c4740d839458
zakisu250 <zakisu250@gmail.com>
2024-01-10 10:51:01 +0300

    Add StudentClass implementation

--- 0x04-TypeScript/task_1/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/task_1/js/main.ts
@@ -15,6 +15,33 @@ interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentInterface;
+}
+
+interface StudentInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
 function printTeacher(firstName: string, lastName: string): string {
   return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+class StudentClass implements StudentInterface {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return 'Currently working';
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
 }
