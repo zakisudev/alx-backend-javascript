33820f5cb786813c43dc1147a68ce88e28cdbf24
zakisu250 <zakisu250@gmail.com>
2024-02-26 18:17:05 +0300

    Refactor HTTP server to use a separate module for reading the database

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -1,70 +1,34 @@
+// 5-http.js
 const http = require('http');
-const fs = require('fs');
-const { argv } = require('process');
-
-function countStudents (path, stream) {
-  if (fs.existsSync(path)) {
-    const data = fs.readFileSync(path, 'utf8');
-    const result = [];
-    data.split('\n').forEach((data) => {
-      result.push(data.split(','));
-    });
-    result.shift();
-    const newis = [];
-    result.forEach((data) => newis.push([data[0], data[3]]));
-    const fields = new Set();
-    newis.forEach((item) => fields.add(item[1]));
-    const final = {};
-    fields.forEach((data) => {
-      final[data] = 0;
-    });
-    newis.forEach((data) => {
-      final[data[1]] += 1;
-    });
-    stream.write(`Number of students: ${result.length}\n`);
-    const temp = [];
-    Object.keys(final).forEach((data) =>
-      temp.push(
-        `Number of students in ${data}: ${final[data]}. List: ${newis
-          .filter((n) => n[1] === data)
-          .map((n) => n[0])
-          .join(', ')}\n`
-      )
-    );
-    // eslint-disable-next-line no-plusplus
-    for (let i = 0; i < temp.length; i++) {
-      if (i === temp.length - 1) {
-        temp[i] = temp[i].replace(/(\r\n|\n|\r)/gm, '');
-      }
-      stream.write(temp[i]);
-    }
-  } else {
-    throw new Error('Cannot load the database');
-  }
-}
-
-const hostname = 'localhost';
-const port = 1245;
+const readDatabase = require('./full_server/utils');
 
 const app = http.createServer((req, res) => {
-  res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  const { url } = req;
-  if (url === '/') {
+  if (req.url === '/') {
     res.write('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    readDatabase('database.csv')
+      .then((students) => {
+        res.write('This is the list of our students\n');
+        for (const field of Object.keys(students).sort()) {
+          res.write(
+            `Number of students in ${field}: ${
+              students[field].length
+            }. List: ${students[field].join(', ')}\n`
+          );
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res.write('Cannot load the database');
+      })
+      .finally(() => res.end());
+  } else {
+    res.write('Page not found');
     res.end();
-  }
-  if (url === '/students') {
-    res.write('This is the list of our students\n');
-    try {
-      countStudents(argv[2], res);
-      res.end();
-    } catch (err) {
-      res.end(err.message);
-    }
   }
 });
 
-app.listen(port, hostname);
+app.listen(1245);
 
 module.exports = app;
