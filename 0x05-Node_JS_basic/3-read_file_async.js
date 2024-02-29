8dff88e446a291f0b559e9c1fbfd6a0ac1732aee
zakisu250 <zakisu250@gmail.com>
2024-02-29 06:17:11 +0300

    Refactor countStudents function to use promises and improve readability

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,46 +1,60 @@
 const fs = require('fs');
 
-async function countStudents (path) {
-  if (fs.existsSync(path)) {
-    return new Promise((resolve) => {
-      fs.readFile(path, 'utf8', (err, data) => {
-        if (err) {
-          throw Error('Cannot load the database');
-        }
-        const result = [];
-        data.split('\n').forEach((data) => {
-          result.push(data.split(','));
-        });
-        result.shift();
-        const newis = [];
-        result.forEach((data) => newis.push([data[0], data[3]]));
-        const fields = new Set();
-        newis.forEach((item) => fields.add(item[1]));
-        const final = {};
-        fields.forEach((data) => {
-          final[data] = 0;
-        });
-        newis.forEach((data) => {
-          final[data[1]] += 1;
-        });
-        console.log(
-          `Number of students: ${
-            result.filter((check) => check.length > 3).length
-          }`
-        );
-        Object.keys(final).forEach((data) =>
-          console.log(
-            `Number of students in ${data}: ${final[data]}. List: ${newis
-              .filter((n) => n[1] === data)
-              .map((n) => n[0])
-              .join(', ')}`
-          )
-        );
-        resolve(result, final, newis);
-      });
+function countStudents(path) {
+  const promise = new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf-8', (error, results) => {
+      if (error) {
+        reject(Error('Cannot load the database'));
+      } else {
+        const lines = results.split('\n');
+        let i = 0;
+        let countStudents = 0;
+        let msg = '';
+        const fields = {};
+
+        const getLines = () => {
+          for (const line of lines) {
+            if (line.trim() !== '' && i > 0) {
+              countStudents += 1;
+              const [fname, lname, age, field] = line.split(','); // eslint-disable-line
+              if (!fields[field]) {
+                fields[field] = {
+                  count: 1,
+                  students: [fname],
+                };
+              } else {
+                const newCount = fields[field].count + 1;
+                const newStudents = fields[field].students.concat(fname);
+                fields[field] = {
+                  count: newCount,
+                  students: newStudents,
+                };
+              }
+            }
+            i += 1;
+          }
+        };
+
+        const display = async () => {
+          getLines();
+          console.log(`Number of students: ${countStudents}`);
+          msg += `Number of students: ${countStudents}\n`;
+          for (const field of Object.keys(fields)) {
+            const n = fields[field].count;
+            const names = fields[field].students.join(', ');
+            console.log(`Number of students in ${field}: ${n}. List: ${names}`);
+            msg += `Number of students in ${field}: ${n}. List: ${names}\n`;
+          }
+          msg = msg.slice(0, -1);
+        };
+
+        display();
+        resolve(msg);
+      }
     });
-  }
-  throw Error('Cannot load the database');
+  });
+
+  return promise;
 }
 
 module.exports = countStudents;
